cd5af816778458f2cee4c545a4dec98e7995c101
Nanda <mariafrsferreira@gmail.com>
2025-02-19 18:02:45 +0000

    Adicionando o programa de login.c

---
+++ aulas/aula03/login.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+  int cpf, senha;
+
+  printf("Digite seu CPF: ");
+  scanf("%d", &cpf);
+
+  printf("Digite sua senha: ");
+  scanf("%d", &senha);
+
+  printf("seu cpf é : %d\n", cpf);
+  printf("sua senha é : %d\n", senha);
+
+  return 0;
+}
