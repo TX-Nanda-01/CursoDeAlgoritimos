b4cec9ddfd5cd84b04ada83247410c1a3f324154
Nanda <mariafrsferreira@gmail.com>
2025-03-12 17:51:41 +0000

    Criado o programa que gera placas

---
+++ aulas/aula5/geradorDePlacas.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.>
+
+
+int main() {
+  char placa[7];
+int i, a, b;
+   srand(time(NULL));)
+
+  for (i = 0; i < 7; i++){
+  // [a,b] ==[65,90]
+    if (i<3){
+      a = 65; 
+      b = 90;
+    placa[i] = rand() % (b -a + 1)) + a;   
+  }  
+  else if (i==3){
+    a = 48; 
+    b = 57;
+  placa[i] = (rand() % (b - a + 1)) + a;
+  }
+   else if (i==4){
+     a = 65; 
+     b = 90;
+     placa[i] = (rand() % (b - a + 1)) + a;
+   }
+    else {
+      a = 48;
+      b = 57;
+      placa[i] = (rand() % (b - a + 1)) + a;
+    }
+    
+    prinf("A placa é: %s\n", placa);
+  
+  return 0;
+}
